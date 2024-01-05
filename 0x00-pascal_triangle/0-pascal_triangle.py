dacc9c3832212e6bdcf5ea36bb0727ebf83f931c
sirln <siro@sirlawren.com>
2024-01-05 18:13:25 +0300

    updating 0-pascal_triangle file

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 Pascal's Triangle
 '''
 
+
 def pascal_triangle(n):
     '''
     Returns a list of lists of integers
@@ -16,12 +17,12 @@ def pascal_triangle(n):
 
     triangle = [[1]]
 
-    for l in range(1, n):
+    for row_index in range(1, n):
         row = [1]
 
         if triangle:
             last_row = triangle[-1]
-            row.extend([sum(pair) for pair in zip (last_row, last_row[1:])])
+            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
 
         row.append(1)
         triangle.append(row)
