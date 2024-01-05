b4abe78639d3c896c4f589bd5e94de3d00e22c24
sirln <siro@sirlawren.com>
2024-01-05 16:36:29 +0300

    Adding 0-pascal_triangle file

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,28 @@
+
+'''
+Pascal's Triangle
+'''
+
+def pascal_triangle(n):
+	triangle = []
+
+	if n <= 0:
+		return triangle
+
+	triangle = [[1]]
+
+	for l in range(1, n):
+		row = [1]
+
+		if triangle:
+			last_row = triangle[-1]
+			row.extend([sum(pair) for pair in zip (last_row, last_row[1:])])
+
+		row.append(1)
+		triangle.append(row)
+
+	return triangle
+
+#if __name__ == "__main__":
+#	plist = pascal_triangle(5)
+#	print(plist)
