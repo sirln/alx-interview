0d99effc9e8e0f4900339b2988c467a21b0605f2
sirln <siro@sirlawren.com>
2024-03-11 01:13:15 +0300

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+
+    return dp[total]
