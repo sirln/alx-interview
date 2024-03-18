613ccd903f397df9ca58cd9c6c7c754432cb2fda
sirln <siro@sirlawren.com>
2024-03-18 10:59:57 +0300

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -37,7 +37,7 @@ def isWinner(x, nums):
     maria_wins = 0
     ben_wins = 0
     for n in nums:
-        primes = sieve_of_eratosthenes(n)
+        primes = sieve_of_eratosthenes(n + 1)
         if can_win(n, primes):
             ben_wins += 1
         else:
