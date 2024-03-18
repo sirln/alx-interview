4af6362f5d8a0f180d126bb197e5539fd570218a
sirln <siro@sirlawren.com>
2024-03-18 12:31:41 +0300

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -12,7 +12,7 @@ def sieve_of_eratosthenes(limit):
     primes = [True for i in range(limit + 1)]
     p = 2
     while (p * p <= limit):
-        if (primes[p] == True):
+        if primes[p]:
             for i in range(p * p, limit + 1, p):
                 primes[i] = False
         p += 1
